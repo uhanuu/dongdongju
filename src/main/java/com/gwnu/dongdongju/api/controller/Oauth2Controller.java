3c8451640dd691d5a51a5adc040ec7ed140678a9
uhanuu <hwryu990927@gmail.com>
2022-10-04 01:40:50 +0900

    카카오톡 로그인 API

--- src/main/java/com/gwnu/dongdongju/api/controller/Oauth2Controller.java
+++ src/main/java/com/gwnu/dongdongju/api/controller/Oauth2Controller.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+
 @Controller
 @RequestMapping("/oauth")
 public class Oauth2Controller {
@@ -17,6 +19,14 @@ public class Oauth2Controller {
         System.out.println("#########" + code);
         String access_Token = kakaoService.getAccessToken(code);
         System.out.println("###access_Token#### : " + access_Token);
+
+        // 위에서 만든 코드 아래에 코드 추가
+        HashMap<String, Object> userInfo = kakaoService.getUserInfo(access_Token);
+        System.out.println("###access_Token#### : " + access_Token);
+        System.out.println("###nickname#### : " + userInfo.get("nickname"));
+        System.out.println("###profile_img#### : " + userInfo.get("profile_Image"));
+        System.out.println("###email#### : " + userInfo.get("email"));
+
         return "testPage";
     }
 }
--- src/main/java/com/gwnu/dongdongju/api/service/KakaoService.java
+++ src/main/java/com/gwnu/dongdongju/api/service/KakaoService.java
@@ -2,6 +2,7 @@ package com.gwnu.dongdongju.api.service;
 
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
 @Service
 @Log4j2
@@ -72,5 +74,52 @@ public class KakaoService {
         }
 
         return access_Token;
+    }
+
+    public HashMap<String, Object> getUserInfo(String access_Token) {
+
+        // 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+        HashMap<String, Object> userInfo = new HashMap<String, Object>();
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            // 요청에 필요한 Header에 포함될 내용
+            conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+            String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+            String profileImage = properties.getAsJsonObject().get("profile_image").getAsString();
+            String email = kakao_account.getAsJsonObject().get("email").getAsString();
+
+            userInfo.put("nickname", nickname);
+            userInfo.put("profile_image", profileImage);
+            userInfo.put("email", email);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return userInfo;
     }
 }
